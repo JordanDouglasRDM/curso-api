a829a1677397a64a3e0988d9a34ca129f0e0dc17
jordan <jordandouglas8515@gmail.com>
2025-04-28 22:37:13 -0300

    feat(userAuth): :sparkles: Criado autenticação com laravel sanctum.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,13 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//stateless e stateful
+
 
 Route::prefix('v1')->group(function () {
 
     /** User CRUD */
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    Route::get('/users', function (Request $request) {
+        $users = User::all();
+
+//        $message = 'Registros encontrados';
+//        if (count($users) === 0) {
+//            $message = 'Nenhum registro encontrado.';
+//        }
+
+        $message = count($users) === 0 ? 'Nenhum registro encontrado' : 'Registros encontrados';
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => $message,
+            'data' => $users
+        ]);
     })->middleware('auth:sanctum');
 
+    Route::post('/login', [UserController::class, 'login']);
 });
 
